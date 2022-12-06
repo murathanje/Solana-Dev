8d9438351895c1030456debce83fec03aaa34103
Murathan Kağan BAYRAM <murathankagan13@gmail.com>
2022-12-06 20:27:35 +0300

    Ping Client done

--- Course/module2.ts
+++ Course/module2.ts
@@ -1,5 +1,5 @@
 // import * as web3 from '@solana/web3.js';
-import { Keypair, PublicKey, Transaction, SystemProgram, LAMPORTS_PER_SOL, sendAndConfirmTransaction, Connection, clusterApiUrl } from '@solana/web3.js';
+import { Keypair, PublicKey, Transaction, SystemProgram, LAMPORTS_PER_SOL, sendAndConfirmTransaction, Connection, clusterApiUrl, TransactionInstruction } from '@solana/web3.js';
 import * as fs from 'fs';
 
 async function main() {
@@ -22,10 +22,13 @@ async function main() {
 
 
     // const secretKey = ownerKeypair.secretKey;
+    const lamports = (LAMPORTS_PER_SOL * 0.1);
+    const transaction = new Transaction();
+    const recipient = new PublicKey("5T9U5g1cbzr2JzCzwTDiDZyT9kcFypQmwDiRH9R9x8y");
+    const instructionData : Buffer = Buffer.alloc(4+8);
+    instructionData.writeInt32LE(2,0);
+    instructionData.writeInt32LE(lamports,4);
 
-    const transaction = new Transaction()
-
-    const recipient = new PublicKey("5T9U5g1cbzr2JzCzwTDiDZyT9kcFypQmwDiRH9R9x8y")
     const sendSolInstruction = SystemProgram.transfer({
         fromPubkey: ownerKeypair.publicKey,
         toPubkey: recipient,
@@ -36,6 +39,26 @@ async function main() {
 
     const connection = new Connection(clusterApiUrl("devnet"));
     // const airdropSignature = await connection.requestAirdrop(ownerKeypair.publicKey, 1*LAMPORTS_PER_SOL);
+    
+    const manualInstruction = new TransactionInstruction({
+        keys:[
+            {
+                pubkey: ownerKeypair.publicKey,
+                isSigner: true,
+                isWritable: true
+            },
+            {
+                pubkey: recipient,
+                isSigner: false,
+                isWritable: true
+            },
+        ],
+        programId: SystemProgram.programId,
+        data: instructionData
+    });
+    
+    transaction.add(manualInstruction);
+    
     const signature = await sendAndConfirmTransaction(
         connection,
         transaction,
@@ -43,6 +66,7 @@ async function main() {
     )
 
     console.log(signature);
+    
 }
 
 main();
